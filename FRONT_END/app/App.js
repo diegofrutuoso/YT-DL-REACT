5f1e8f0a65ef324360d7c9122859805af2fe45b2
Diego Frutuoso Machado <dfrutuoso_machado@hotmail.com>
2017-06-26 18:44:54 -0300

    Fix wrong dependencies

--- FRONT_END/app/App.js
+++ FRONT_END/app/App.js
@@ -1,4 +1,4 @@
-var React = require('../bower_components/react'); // importa a lib react
+var React = require('./bower_components/react'); // importa a lib react
 var reactDOM = require('react-dom'); // importa a lib react-dom
 
 var Youtube = require('./components/Youtube');
